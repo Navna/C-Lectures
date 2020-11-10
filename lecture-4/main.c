0f62666dbf54fbd0210fab1cd8bb959154d81967
Ruslan Yuzkiv <yuzkiv@geosamara.ru>
2020-11-10 19:28:57 +0400

    Лекция 4: Статические массивы и обратный порядок байтов

--- lecture-4/main.c
+++ lecture-4/main.c
@@ -27,6 +27,31 @@ float PowerN(const float x, const int n) {
     }
 }
 
+void print(const int ptr[16], const size_t n) {
+    printf("print sizeof = %zu\n", sizeof(ptr) / sizeof(*ptr));
+
+    for (size_t i = 0; i < n; ++i) {
+        printf("%d ", *(ptr + i));
+    }
+    printf("\n");
+}
+
 int main(void) {
+    // Пример изменения адреса локальной переменной на стеке при рекурсивных вызовах
     PowerN(2, -5);
+    printf("\n");
+
+    // Пример статического массива
+    int data[8] = { 7,6,5,4,3,2,1,0 };
+    const size_t n = sizeof(data) / sizeof(*data);
+    printf("main sizeof = %zu\n", n);
+    print(data, n);
+    printf("\n");
+
+    // Демонстрация обратного порядка байтов
+    long i = 11394560;
+    unsigned char* ptr = (unsigned char*)&i;
+    // DEC        HEX (Big-Endian)   HEX (Little-Endian)
+    // 14593280   00 AD DE 00        00 DE AD 00
+    printf("%02X %02X %02X %02X\n\n", ptr[0], ptr[1], ptr[2], ptr[3]);
 }
