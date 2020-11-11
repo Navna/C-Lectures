0e771d2be2edda1949b1fd9c06a3580eb80eac78
Ruslan Yuzkiv <yuzkiv@geosamara.ru>
2020-11-11 20:06:29 +0400

    Лекция 5: Строки

---
+++ lecture-5/main.c
@@ -0,0 +1,45 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* my_strtok(char* str, const char* delim) {
+    static char* context = NULL;
+    if (str == NULL)
+        str = context;
+    if (str == NULL) {
+        return NULL;
+    }
+
+    // TODO
+    context = NULL;
+}
+
+int main(void) {
+    char c = 13;
+    char c = '\t';
+
+    // Демонстрация strtok
+    const char* srcSentence = "Don't tease! Well, and what has been decided about No - vosiltsev's dispatch? You know everything.";
+    const char* delimiters = "!, -?";
+    char* sentence = malloc(strlen(srcSentence) + (size_t)1);
+    if (sentence == NULL)
+        return EXIT_FAILURE;
+    strcpy(sentence, srcSentence);
+    printf("%s\n\n", sentence);
+
+    char* r = strtok(sentence, delimiters);
+    while (r != NULL)
+    {
+        printf("%s\n", r);
+        r = strtok(NULL, delimiters);
+    }
+    printf("\n");
+
+    // Пример переполнения
+    const char* str = "2147483648";
+    errno = 0;
+    const long value = strtol(str, NULL, 0);
+    printf("%d\n", errno == ERANGE);
+    printf("%ld", value);
+}
